aa2b53e01ac8d3d85be1d561003fbe977b723e1d
davidcedrik95 <moukam2@gmail.com>
2026-03-28 22:59:02 +0100

    update

---
+++ data table/feed_consumption_insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO feed_consumption (pig_id, period, feed_type, quantity, cost, conversion) VALUES
+('P001', 'Mar 2026', 'Croissance', 77.5, 19375.00, 3.13),
+('P002', 'Mar 2026', 'Gestation', 99.2, 29760.00, NULL),
+('P003', 'Mar 2026', 'Reproduction', 108.5, 30380.00, NULL),
+('P004', 'Mar 2026', 'Porcelet', 24.8, 8680.00, 2.67);
